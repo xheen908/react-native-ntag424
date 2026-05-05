ba75ba81e9b78d8eed2596baf09cff9e701b2129
Arndt Christoph Handschuh <xheen908@MBP-von-Arndt.fritz.box>
2026-05-06 00:05:58 +0200

    Remove unused imports from index.ts to fix SDK 51+ compatibility

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import { EventEmitter } from 'expo-modules-core';
 
 // Import the native module. On web, it will be null.
 import VLedgerNtagModule from './src/VLedgerNtagModule';
